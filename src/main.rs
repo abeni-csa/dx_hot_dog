914ba7957d9bfc0ec92632336ef562f0a0032dc4
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-23 13:22:31 +0300

    Encapsulating State

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
 static CSS: Asset = asset!("/assets/main.css");
-static ICON: Asset = asset!("/assets/icon.ico", ImageAssetOptions::new().with_avif());
+// static ICON: Asset = asset!("/assets/icon.ico", ImageAssetOptions::new().with_avif());
 fn main() {
     dioxus::launch(App);
 }
@@ -8,20 +8,38 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Stylesheet{ href: CSS }
-        div {
-             id: "title",
-              h1 { "Hot Dog" }
-          }
+        document::Stylesheet { href: CSS }
+        Title {}
+        DogView {}
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
+     div {
+         id: "title",
+         h1 { "HOT DOG" }
+      }
+    }
+}
+#[component]
+fn DogView() -> Element {
+    let mut img_src = use_signal(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
+    let skip = move |env| {};
+    let save = move |env| {
+        img_src.set("https://images.dog.ceo/breeds/briard/n02105251_6984.jpg");
+    };
+    rsx! {
         div {
 
              id: "dogview",
-            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg"}
+            img { src: "{img_src}"}
 
          }
          div { id: "buttons",
-             button { id: "save", "Save" }
-             button { id: "skip", "Skip" }
+             button { onclick:skip, id: "save", "Save" }
+             button { onclick:save, id: "skip", "Skip" }
          }
 
     }
