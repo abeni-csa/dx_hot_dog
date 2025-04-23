c5799e45a8d38635cfd04ae729cf4eec302c750d
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-23 09:58:12 +0300

    Hello world and rsx!() macro procaticed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use global_attributes::user_select;
 
 fn main() {
     dioxus::launch(App);
@@ -6,5 +7,50 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! {
+         div {
+            color: "red",
+            "Front end with out Js!",
+            button { "Click me!"}
+        }
+    };
+    rsx! {
+         div {  color: "green", "somethings" }
+        ol {
+            {(0..10).map(|i| rsx!{ "{i} [name]" })}
+         }
+    };
+    let show_titile = true;
+    let users = vec!["Abeni", "nahom", "nati", "Jemil", "abi"];
+    rsx! {
+        if show_titile {
+            h1 { "Hellow world" }
+        }
+        for item in 0..10 {
+             ol {
+                 "{item}"
+             }
+        }
+        for ( id,name ) in users.iter().enumerate() {
+            div {
+                ol {
+                "{id}",
+                "{name}"
+            }
+
+            }
+        }
+    }
 }
+// rsx! {
+//     // Anything that's `Display`
+//     {"Something"}
+//
+//     // Optionals
+//     {show_title.then(|| rsx! { "title!" } )}
+//
+//     // And iterators
+//     ul {
+//         {(0..5).map(|i| rsx! { "{i}" })}
+//     }
+// }
